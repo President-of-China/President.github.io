f4b849fe0c2ca81d4ea2619809be6af406f84a24
President-of-China <chinatelecom.me@gmail.com>
2019-12-09 18:00:34 +0800

    xx

--- chunks/gui.js
+++ chunks/gui.js
@@ -147,7 +147,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
 
 var onClickLogo = function onClickLogo() {
-  window.open = 'https://scratch.mit.edu';
+  window.open('https://fengke.best','_blank');
 };
 
 var handleTelemetryModalCancel = function handleTelemetryModalCancel() {
